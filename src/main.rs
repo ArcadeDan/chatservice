4604c213e9402098b4d6ab9ae8ab4da56331f1a0
Dan <lordaniel24@gmail.com>
2026-03-18 12:36:01 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{
+    fs, io::{BufRead, BufReader, Write}, os::unix::net::{self, UnixListener, UnixStream}, sync::{Arc, Mutex}, thread
+};
+
 fn main() {
-    println!("Hello, world!");
+    let socket_path = "/tmp/chatservice.sock";
+    let _ = fs::remove_file(socket_path); // remove socket from last session
+
+    // creates socket file and listens for incoming connections
+    let listener = UnixListener::bind(socket_path).expect("Failed to bind to socket");
+    listener.set_nonblocking(true).expect("Failed to set non-blocking mode");
+    
+    println!("Chat server listening on {}", socket_path);
+
+    let mut clients: Vec<UnixStream> = Vec::new();
+
+    loop {
+        match listener.accept() {
+            Ok((stream, _addr)) => {
+                println!("New client connected");
+            
+            }
+        }
+    }
+
+
+
+
+    
+             
 }
